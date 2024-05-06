d8764ee599d9462ab4e6b4b1c0a4b32645f7525f
Dawid Miklas <miklasdawid@gmail.com>
2024-05-07 00:28:34 +0200

    build fix

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,15 +4,15 @@ import '../css/app.css';
 import {createApp, DefineComponent, h} from 'vue';
 import {createInertiaApp} from '@inertiajs/vue3';
 import {ZiggyVue} from "../../vendor/tightenco/ziggy";
+import {resolvePageComponent} from "laravel-vite-plugin/inertia-helpers";
 import BaseLayout from "@/Layouts/BaseLayout.vue";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name): Promise<DefineComponent> | DefineComponent | { default: DefineComponent } => {
-        const pages = import.meta.glob('./Pages/**/*.vue', {eager: true});
-        let page = pages[`./Pages/${name}.vue`] as { default: DefineComponent };
+    resolve: async name => {
+        const page = await resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue'));
         page.default.layout = page.default.layout || BaseLayout;
         return page;
     },
